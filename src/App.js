1ff41267410f75e0993c8cdcbeeb70547ab64631
dancsgergo <dancs.gergely@gmail.com>
2024-10-04 23:00:41 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import menuImage from './menu2.jpg'
+
+function App() {
+  return (
+    <div className="App">
+        <img src={menuImage} alt='menu'/>
+
+    </div>
+  );
+}
+
+export default App;
